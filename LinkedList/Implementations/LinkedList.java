d5fbf172573a4458e10e8a80bb48cb151516874d
shubh08 <shubamkumar567@gmail.com>
2018-12-14 22:58:23 +0530

    Add element variation and remove added

--- LinkedList/Implementations/LinkedList.java
+++ LinkedList/Implementations/LinkedList.java
@@ -3,38 +3,55 @@ package com.shubh.linkedList;
 public class LinkedList {
 
 	Node root;
-	
-	public void add(int data){
-		
+
+	// Adding Element at the beginning
+	public void add(int data) {
+
 		Node dataToAdd = new Node(data);
-		
-		if(root==null)
-		{
+
+		if (root == null) {
 			root = dataToAdd;
 		}
-		
+
 		else
 			insertAtBegining(dataToAdd);
-		
-		
+
 	}
 
 	private void insertAtBegining(Node dataToAdd) {
 		dataToAdd.next = root;
 		root = dataToAdd;
-		
+
 	}
-	
-	public void traversePrint(){
-		
+
+	// Traversing the List
+	public void traversePrint() {
+
 		Node temp = root;
-		
-		while(temp != null)
-		{
-			System.out.print(temp.data+"-->");
+
+		while (temp != null) {
+			System.out.print(temp.data + "-->");
 			temp = temp.next;
 		}
 		System.out.println("null");
 	}
+
+	// Adding Element at the End
+	public void addAtEnd(int data, Node current) {
+		if (current.next != null)
+			addAtEnd(data, current.next);
+		else {
+			Node newNode = new Node(data);
+			current.next = newNode;
+		}
+	}
+
+	// Removing element
+	public void remove (int data)
+	{
+	
+		
+	}
+	
 	
 }
