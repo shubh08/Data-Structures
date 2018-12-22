354714949c31a5b0b2d1c89a821f1b83a615c0b7
Shubham Kumar <shubhamkumar567@gmail.com>
2018-12-23 00:07:54 +0530

    Update the Delete file for Doubly Linked List

--- Data Structures/Linked List/Implementations/DeleteNodeInDoublyLinnkedList.java
+++ Data Structures/Linked List/Implementations/DeleteNodeInDoublyLinnkedList.java
@@ -3,16 +3,27 @@ class GfG
     // function returns the head of the linkedlist
     Node deleteNode(Node head,int x)
     {
-	 if(head == null) return;
+	 
+	 if(head == null) return null;
 	 
 	 if(x==1) return head.next;
 	 int position =1;
 	 Node headRef = head;
 	 while(head != null)
-	    if(position = x)
+	    if(position == x)
 	    {
-	        head
+	        head.prev.next = head.next;
+	        if(head.next!= null)
+	        head.next.prev = head.prev;
+	    
+	        break;
 	    }
+	 else{
+	     head = head.next;
+	     position++;
+	 }
+	 return headRef;
+    
 	 
     }
 }
