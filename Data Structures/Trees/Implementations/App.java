fa5cd07535f8424d65b941a3887782007ff6ed77
shubh08 <shubhamkumar567@gmail.com>
2019-02-16 19:27:55 +0530

    Added implementations for Tree Data Structure

---
+++ Data Structures/Trees/Implementations/App.java
@@ -0,0 +1,21 @@
+package com.shubh.bst;
+
+public class App {
+
+	public static void main(String[] args) {
+		BinarySearchTree bst = new BinarySearchTree();
+		bst.insert(10);
+		bst.insert(2);
+		bst.insert(5);
+		bst.insert(15);
+		bst.insert(20);
+		
+		System.out.println("The min element is "+bst.getMin());
+
+		System.out.println("The max element is "+bst.getMax());
+	}
+	
+
+	
+	
+}
---
+++ Data Structures/Trees/Implementations/BinarySearchTree.java
@@ -0,0 +1,72 @@
+package com.shubh.bst;
+
+public class BinarySearchTree {
+
+	private Node rootNode;
+
+	public void insert(int data) {
+
+		
+		
+		
+		if (rootNode == null) {
+			rootNode = new Node(data);
+		} else
+			insertNode(data, rootNode);
+
+	}
+	
+	public int getMin(){
+		if (rootNode==null)
+			return -1;
+		else
+			return getMinNode(rootNode);
+		
+	}
+	
+	public int getMax(){
+		if (rootNode==null)
+			return -1;
+		else
+			return getMaxNode(rootNode);
+		
+	}
+	
+	private int getMinNode(Node node) {
+		if(node.getLeftChild()!=null)
+			return getMinNode(node.getLeftChild());
+		
+		return node.getData();
+	}
+
+
+	private int getMaxNode(Node node) {
+		if(node.getRightChild()!=null)
+			return getMaxNode(node.getRightChild());
+		
+		return node.getData();
+	}
+
+	public void insertNode(int newData, Node Node)
+
+	{
+		if (newData<Node.getData()) {
+			if (Node.getLeftChild() != null)
+				insertNode(newData, Node.getLeftChild());
+			else {
+				Node newNode = new Node(newData);
+				Node.setLeftChild(newNode);
+
+			}
+		} else {
+			if (Node.getRightChild() != null)
+				insertNode(newData, Node.getRightChild());
+			else {
+				Node newNode = new Node(newData);
+				Node.setRightChild(newNode);
+
+			}
+		}
+	}
+
+}
