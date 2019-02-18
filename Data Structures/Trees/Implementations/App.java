41a44788d5e45ea044fca3ee5f5351b7dc3cb294
shubh08 <shubhamkumar567@gmail.com>
2019-02-18 22:26:01 +0530

    Added pre and post order traversals

--- Data Structures/Trees/Implementations/App.java
+++ Data Structures/Trees/Implementations/App.java
@@ -5,14 +5,16 @@ public class App {
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
 		bst.insert(10);
-		bst.insert(2);
 		bst.insert(5);
 		bst.insert(15);
-		bst.insert(20);
+		bst.insert(3);
+		bst.insert(7);
 		
 		System.out.println("The min element is "+bst.getMin());
 
 		System.out.println("The max element is "+bst.getMax());
+		bst.traverse();
+		bst.delete(10);
 		bst.traverse();
 	}
 	
--- Data Structures/Trees/Implementations/BinarySearchTree.java
+++ Data Structures/Trees/Implementations/BinarySearchTree.java
@@ -12,23 +12,42 @@ public class BinarySearchTree {
 			insertNode(data, rootNode);
 
 	}
-	
-	public void traverse()
-	{
-		if(rootNode!=null)
-		{
+
+	public void traverse() {
+		if (rootNode != null) {
 			inOrderTraversal(rootNode);
+			preOrderTraversal(rootNode);
+			postOrderTraversal(rootNode);
 		}
 	}
 
+	private void postOrderTraversal(Node node) {
+		if (node.getLeftChild() != null)
+			postOrderTraversal(node.getLeftChild());
+		if (node.getRightChild() != null)
+			postOrderTraversal(node.getRightChild());
+		System.out.print(node.getData() + "-->");
+	}
+
+	private void preOrderTraversal(Node node) {
+
+		System.out.print(node.getData() + "-->");
+		if (node.getLeftChild() != null)
+			preOrderTraversal(node.getLeftChild());
+
+		if (node.getRightChild() != null)
+			preOrderTraversal(node.getRightChild());
+	}
+
+	// InOrder Traversal
 	private void inOrderTraversal(Node node) {
 
-		if(node.getLeftChild()!=null)
+		if (node.getLeftChild() != null)
 			inOrderTraversal(node.getLeftChild());
-		System.out.print(node.getData()+"-->");
-		if(node.getRightChild()!=null)
+		System.out.print(node.getData() + "-->");
+		if (node.getRightChild() != null)
 			inOrderTraversal(node.getRightChild());
-		
+
 	}
 
 	public int getMin() {
@@ -81,6 +100,55 @@ public class BinarySearchTree {
 
 			}
 		}
+	}
+
+	public void delete(int data) {
+
+		if (rootNode != null)
+			rootNode = deleteNode(rootNode, data);
+	}
+
+	private Node deleteNode(Node node, int data) {
+		if (node == null)
+			return node;
+
+		if (data < node.getData())
+			node.setLeftChild(deleteNode(node.getLeftChild(), data));
+		else if (data > node.getData())
+			node.setRightChild(deleteNode(node.getRightChild(), data));
+		else {
+			// We found the node to be removed
+			if (node.getLeftChild() == null && node.getRightChild() == null) {
+				System.out.println("Removing a leaf node!!");
+				return null;
+			}
+
+			if (node.getLeftChild() == null) {
+				System.out.println("Removing the right child!!");
+				Node tempNode = node.getRightChild();
+				node = null;
+				return tempNode;
+			} else if (node.getRightChild() == null) {
+				System.out.println("Removing the left child!!");
+				Node tempNode = node.getLeftChild();
+				node = null;
+				return tempNode;
+			}
+
+			System.out.println("Removing item with two children!!");
+			Node tempNode = getPredecessor(node.getLeftChild());
+			node.setData(tempNode.getData());
+			node.setLeftChild(deleteNode(node.getLeftChild(), tempNode.getData()));
+		}
+		return node;
+	}
+
+	private Node getPredecessor(Node node) {
+		// TODO Auto-generated method stub
+
+		if (node.getRightChild() != null)
+			return getPredecessor(node.getRightChild());
+		return node;
 	}
 
 }
