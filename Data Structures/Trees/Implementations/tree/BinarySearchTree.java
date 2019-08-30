7e8797db248b014c856b5716610725e1cb480be2
shubh08 <shubhamkumar567@gmail.com>
2019-08-30 00:27:21 -0700

    Added implementation for Zig Zag traveral

--- Data Structures/Trees/Implementations/tree/BinarySearchTree.java
+++ Data Structures/Trees/Implementations/tree/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package com.shubh.tree;
 
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BinarySearchTree {
@@ -130,6 +135,12 @@ public class BinarySearchTree {
 		System.out.println("In Order Traversal !!");
 		tree.inOrder();
 		tree.inOrderIterative();
+		tree.levelOrderInit();
+		tree.levelOrderTraversalLevelByLevel();
+		System.out.println("Zig Zag Traversal!!");
+		tree.zigZagTraversal();
+		System.out.println("Level Order Traversal Single Line");
+		tree.levelOrderTraversalSingleLine();
 		System.out.println("\n Deleting 20 ");
 		tree.deleteRec(20);
 		System.out.println("In Order Traversal!!");
@@ -141,6 +152,36 @@ public class BinarySearchTree {
 
 	}
 
+	public void zigZagTraversal() {
+		Node tempRoot = root;
+		if(tempRoot==null) return;
+		Stack<Node> s1 = new Stack<>();
+		Stack<Node> s2 = new Stack<>();
+		s1.push(tempRoot);
+		while(!s1.isEmpty() || !s2.isEmpty())
+		{
+			while(!s1.isEmpty())
+			{	Node x = s1.pop();
+				System.out.print(x.data+"-->");
+				if(x.right!=null)
+				s2.push(x.right);
+				if(x.left!=null)
+				s2.push(x.left);
+			}
+			
+			while(!s2.isEmpty())
+			{	Node x = s2.pop();
+				System.out.print(x.data+"-->");
+				if(x.left!=null)
+				s1.push(x.left);
+				if(x.right!=null)
+				s1.push(x.right);
+			}
+		}
+		
+		
+	}
+	
 	private void deleteRec(int key) {
 
 		root = deleteRecord(root, key);
@@ -183,4 +224,104 @@ public class BinarySearchTree {
 		return returnKey;
 	}
 
+	
+	public void levelOrderTraversalSingleLine()
+	{
+		Node rootDup = root;
+		if(root==null) return;
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(rootDup);
+		System.out.println("Level Order Traversal Single Line!!");
+		while(!queue.isEmpty())
+		{
+			Node x = queue.poll();
+			System.out.println(x.data);
+			if(x.left!=null) queue.add(x.left);
+			if(x.right!=null) queue.add(x.right);
+		}
+		
+	}
+	
+public void levelOrderInit() {
+	levelOrder(root);
+}	
+	
+public List<List<Integer>> levelOrder(Node root) {
+        
+        List<List<Integer>> masterList = new ArrayList<>();
+        
+        if(root==null) return masterList;
+        
+        Queue<Node> qu = new LinkedList<>();
+        qu.add(root);
+        while(!qu.isEmpty())
+        {
+            List<Integer> temp = new ArrayList<>();
+             
+            for(int i=0;i<qu.size();i++)
+            {
+            	Node x = qu.remove();
+                temp.add(x.data);
+                if(x.left!=null)
+                {  qu.add(x.left);}
+                if(x.right!=null)
+                { qu.add(x.right);}
+            }
+            
+            masterList.add(temp);
+            
+        }
+        
+        return masterList;
+    }
+	
+
+
+	public void levelOrderTraversalLevelByLevel() {
+
+		Node tempRoot = root;
+		Queue<Node> queue = new LinkedList<>();
+		if (tempRoot == null)
+			return;
+		//List<List<Integer>> masterListd =  new ArrayList<>();;
+		ArrayList<ArrayList<Node>> masterList = new ArrayList<ArrayList<Node>>();
+		ArrayList<Node> rootTemp = new ArrayList<Node>();
+		rootTemp.add(tempRoot);
+		masterList.add(rootTemp);
+		rootTemp.clear();
+		queue.add(tempRoot);
+		queue.add(null);
+		boolean loop = true;
+		int flag = 0;
+		while (!queue.isEmpty()) {
+
+			Node x = queue.poll();
+
+			if (x == null) {
+				if (flag == 0)
+					break;
+				queue.add(null);
+				
+				masterList.add(rootTemp);
+				rootTemp.clear();
+				flag = 0;
+			}
+
+			else {
+				flag = 1;
+				if (x.left != null) {
+					rootTemp.add(x.left);
+					queue.add(x.left);
+				}
+
+				if (x.right != null) {
+					rootTemp.add(x.right);
+					queue.add(x.right);
+				}
+
+			}
+
+		}
+		System.out.println("Printing ArrayList" + masterList);
+	}
 }
