2d601e44af972e7980049d8a9e5aa06e1484a135
shubh08 <shubhamkumar567@gmail.com>
2019-09-07 14:16:45 -0700

    Added left view,topview ,right view of a tree implementation

--- Data Structures/Trees/Implementations/tree/BinarySearchTree.java
+++ Data Structures/Trees/Implementations/tree/BinarySearchTree.java
@@ -2,8 +2,10 @@ package com.shubh.tree;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -21,9 +23,55 @@ public class BinarySearchTree {
 		}
 
 	}
+	
+	 class QueueNode{
+	        int hd;
+	        Node node;
+	        
+	       public QueueNode(int h, Node nod){
+	            
+	            hd = h;
+	            node = nod;
+	            
+	        }
+	    }
 
 	Node root;
 
+	//Print Top View of the Binary Tree
+	
+	public void printTopView() {
+		
+		
+if(root==null) return;
+        
+        Queue<QueueNode> q = new LinkedList<>();
+        
+        Map<Integer,Node> map = new HashMap<>();
+        q.add(new QueueNode(0,root));
+        
+        while(!q.isEmpty())
+        {
+           QueueNode temp = q.poll();
+           if(!map.containsKey(temp.hd))
+           {
+               System.out.print(" "+temp.node.data);
+               map.put(temp.hd,temp.node);
+               
+           }
+           
+           if(temp.node.left!=null)
+           q.add(new QueueNode(temp.hd-1,temp.node.left));
+           
+           if(temp.node.right!=null)
+           q.add(new QueueNode(temp.hd+1,temp.node.right));
+           
+            
+            
+        }
+		
+	}
+	
 	BinarySearchTree() {
 		root = null;
 	}
@@ -32,6 +80,42 @@ public class BinarySearchTree {
 		root = insertRec(root, key);
 	}
 
+	public void printLeftView() {
+		Queue<Node> q = new LinkedList<Node>();  
+        q.add(root); 
+       
+        // null marker to represent end of current level 
+        q.add(null);  
+        boolean printedFirst = false;
+        // Do Level order of tree using NULL markers 
+        while (!q.isEmpty()) { 
+           
+        	Node p = q.peek(); 
+            q.remove(); 
+            if(!printedFirst && p!=null)
+            	{System.out.print(" "+p.data);
+            	printedFirst = true;
+            	}
+            
+            if (p != null) { 
+                // push left and right children of current 
+                // node 
+                if (p.left != null) 
+                    q.add(p.left);  
+                if (p.right != null) 
+                    q.add(p.right); 
+            }  
+              
+            // if queue is not empty, push NULL to mark  
+            // nodes at this level are visited 
+            else if (!q.isEmpty())  {
+            	 q.add(null);  
+            	 printedFirst = false;
+            }
+               
+        } 
+	}
+	
 	private Node insertRec(Node root, int key) {
 		if (root == null) {
 			root = new Node(key);
@@ -123,6 +207,29 @@ public class BinarySearchTree {
 		System.out.print(root.data + "-->");
 	}
 
+	 public int kthSmallest( int k) {
+	        Node temp = root;
+	        Stack<Node> st = new Stack<>();
+	        
+	        while(true)
+	        {
+	            if(temp!=null)
+	            {
+	                st.push(temp);
+	                temp = temp.left;
+	            }
+	            
+	            if(st.isEmpty())
+	                break;
+	            k--;
+	            temp = st.pop();
+	            if(k==0) return temp.data;
+	            temp = temp.right;
+	            
+	        }
+	        
+	        return 0;
+	    }
 	public static void main(String[] args) {
 		BinarySearchTree tree = new BinarySearchTree();
 		tree.insert(50);
@@ -132,8 +239,16 @@ public class BinarySearchTree {
 		tree.insert(70);
 		tree.insert(60);
 		tree.insert(80);
+		
+		System.out.println("Printing the leftView of the binaryTree");
+		tree.printLeftView();
+		
+		System.out.println("Printing Top View of the Tree");
+		tree.printTopView();
+		
 		System.out.println("In Order Traversal !!");
 		tree.inOrder();
+		System.out.println("Printing 2nd smallest element "+tree.kthSmallest(2));
 		tree.inOrderIterative();
 		tree.levelOrderInit();
 		tree.levelOrderTraversalLevelByLevel();
@@ -157,29 +272,40 @@ public class BinarySearchTree {
 		if(tempRoot==null) return;
 		Stack<Node> s1 = new Stack<>();
 		Stack<Node> s2 = new Stack<>();
+		List<List<Integer>>  masterList = new ArrayList<>();
 		s1.push(tempRoot);
 		while(!s1.isEmpty() || !s2.isEmpty())
 		{
+			List<Integer> listS1 = new ArrayList<Integer>();
 			while(!s1.isEmpty())
-			{	Node x = s1.pop();
+			{	
+				Node x = s1.pop();
+				listS1.add(x.data);
 				System.out.print(x.data+"-->");
 				if(x.right!=null)
 				s2.push(x.right);
 				if(x.left!=null)
 				s2.push(x.left);
 			}
+			if(listS1.size()>0)
+				masterList.add(listS1);
+			
+			List<Integer> listS2 = new ArrayList<Integer>();
 			
 			while(!s2.isEmpty())
 			{	Node x = s2.pop();
+			listS2.add(x.data);
 				System.out.print(x.data+"-->");
 				if(x.left!=null)
 				s1.push(x.left);
 				if(x.right!=null)
 				s1.push(x.right);
 			}
+			if(listS2.size()>0)
+				masterList.add(listS2);
 		}
 		
-		
+		System.out.println("Master List Content-->"+masterList);
 	}
 	
 	private void deleteRec(int key) {
